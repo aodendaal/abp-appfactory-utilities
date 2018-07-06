8a84901e71fb5105f1354d2499dbba9930b6ce6e
Andre Odendaal <aodendaal@gmail.com>
2018-07-06 17:03:25 +0200

    Initial Commit

---
+++ Abp.AppFactory.Utilities/UrlPath.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Abp.AppFactory.Utilities
+{
+    public static class UrlPath
+    {
+        private const char pathSeparator = '/';
+
+        public static string Combine(string path1, string path2)
+        {
+            if (path1 == null)
+            {
+                throw new ArgumentNullException("path1");
+            }
+
+            if (path2 == null)
+            {
+                throw new ArgumentNullException("path2");
+            }
+
+            if (path1.Length == 0)
+            {
+                return path2;
+            }
+
+            if (path2.Length == 0)
+            {
+                return path1;
+            }
+
+            var p1end = path1[path1.Length - 1];
+            if (p1end != pathSeparator)
+            {
+                return path1 + pathSeparator + path2;
+            }
+
+            return path1 + path2;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# abp-appfactory-utilities
+Utilities and common functions for using ASP.NET Boilerplate AppFactory modules
