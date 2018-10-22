1fa1dde4c5da0009081bde3fd4e89a0312dfc573
Andre Odendaal <aodendaal@gmail.com>
2018-10-22 15:10:59 +0200

    Add EntityNotFoundException and string extensions

---
+++ Abp.AppFactory.Utilities/EntityNotFoundException.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abp.AppFactory.Utilities
+{
+    public class EntityNotFoundException : Exception
+    {
+        private string field;
+        private string value;
+        private string entityType;
+
+        public override string Message
+        {
+            get
+            {
+                if (field == null && value == null)
+                {
+                    return $"Entity '{entityType}' not found";
+                }
+                else if (field == null)
+                {
+                    return $"Entity '{entityType}' with Id '{value}' not found";
+                }
+                else
+                {
+                    return $"Entity '{entityType}' with '{field}' = '{value}' not found";
+                }
+            }
+        }
+
+        public EntityNotFoundException(Type entityType)
+        {
+            this.entityType = entityType.Name;
+        }
+
+        public EntityNotFoundException(Type entityType, long id)
+        {
+            this.entityType = entityType.Name;
+            this.value = id.ToString();
+        }
+
+        public EntityNotFoundException(Type entityType, string propertyName, string value)
+        {
+            this.entityType = entityType.Name;
+            this.field = propertyName;
+            this.value = value;
+        }
+    }
+}
---
+++ Abp.AppFactory.Utilities/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abp.AppFactory.Utilities
+{
+    public static class StringExtensions
+    {
+        public static byte[] ToByteArray(this string input)
+        {
+            var encoding = new UnicodeEncoding();
+            var bytes = encoding.GetBytes(input);
+            var uf16bytes = new List<byte>();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    uf16bytes.Add(bytes[i]);
+                }
+            }
+
+            return uf16bytes.ToArray();
+        }
+    }
+}
